9ade7051f319c96f15ef6a851a6733e5e7f05721
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-19 17:53:09 +0500

    Update vars.py

--- main/vars.py
+++ main/vars.py
@@ -14,7 +14,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minute
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL","-1001843564893", None)
+        environ.get("BIN_CHANNEL","-1001843564893")
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
